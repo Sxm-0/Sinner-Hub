bad4f3fa266db37dbc957a2127d3781af7314292
Why <95942743+Why3005@users.noreply.github.com>
2023-01-22 16:35:33 +0000

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,1 +1,8 @@
+loadstring(game:HttpGet("https://github.com/Why3005/Roblox/raw/main/System%20Messages",true))()
 
+local SupportedGameIds = {4483381587}
+if table_contains(SupportedGamesIds,game.PlaceId) then
+  SendSystemMessage("Yes")
+else
+  SendSystemMessage("This game is not supported")
+end
