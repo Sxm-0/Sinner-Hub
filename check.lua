73096c32be9bfe6290de8cb0b92b1ec5d3581eae
Why <95942743+Why3005@users.noreply.github.com>
2023-01-22 18:08:25 +0000

    Update check.lua

--- check.lua
+++ check.lua
@@ -3,7 +3,8 @@ loadstring(game:HttpGet("https://github.com/Why3005/Roblox/raw/main/System%20Mes
 SupportedGames = {}
 
 if table.find(SupportedGames, tostring(game.PlaceId)) ~= nil then
-  SendSystemMessage("This Game Is Supported")
+  SendSystemMessage("Loaded Sinnner Hub")
+  loadstring(game:HttpGet("https://github.com/Why3005/Sinner-Hub/raw/main/check.lua", true))()
 else
   SendSystemMessage("This game is not supported")
   game.Players.LocalPlayer:Kick("Unsupported Game, DM Why#3005 for a chance to get your game added.")
